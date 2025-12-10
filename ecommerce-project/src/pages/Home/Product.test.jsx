a8a9666062a022cda9b752308c93c483eae7bd8a
Vinoth Srinivasan <vinothsrinivasan372@gmail.com>
2025-12-11 00:33:28 +0530

    Test Clicking Add to cart button

--- ecommerce-project/src/pages/Home/Product.test.jsx
+++ ecommerce-project/src/pages/Home/Product.test.jsx
@@ -1,10 +1,15 @@
 import { it, expect, describe, vi } from 'vitest';
 import { Product } from './Product.jsx';
-import { render ,screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+
+
+vi.mock('axios');
 
 describe('Product Component', () => {
 
-    it ('displays the product details correctly', () => {
+    it('displays the product details correctly', () => {
 
         const product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -24,25 +29,59 @@ describe('Product Component', () => {
 
         expect(
             screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs'))
-        .toBeInTheDocument();
+            .toBeInTheDocument();
 
         expect(
             screen.getByText('$10.90'))
-        .toBeInTheDocument();
+            .toBeInTheDocument();
 
         expect(
             screen.getByText('87'))
-        .toBeInTheDocument();
+            .toBeInTheDocument();
 
         expect(
             screen.getByTestId('product-image')
-        ).toHaveAttribute('src','images/products/athletic-cotton-socks-6-pairs.jpg');
+        ).toHaveAttribute('src', 'images/products/athletic-cotton-socks-6-pairs.jpg');
 
         expect(
-            screen.getByTestId('product-rating-stars-image')  
-        ).toHaveAttribute('src','images/ratings/rating-45.png');
+            screen.getByTestId('product-rating-stars-image')
+        ).toHaveAttribute('src', 'images/ratings/rating-45.png');
 
     });
+
+    it('adds a product to the cart',async () => {
+        const product = {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+            rating: {
+                stars: 4.5,
+                count: 87
+            },
+            priceCents: 1090,
+            keywords: ["socks", "sports", "apparel"]
+        };
+
+        const loadCart = vi.fn();//Create a mock function
+
+        render(<Product product={product} loadCart={loadCart} />);
+
+        const user = userEvent.setup();
+
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+
+        await user.click(addToCartButton);
+
+
+        expect(axios.post).toHaveBeenCalledWith('/api/cart-items',
+            {
+            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 1
+        });
+
+        expect(loadCart).toHaveBeenCalled();
+
+    })
 
 });
 
